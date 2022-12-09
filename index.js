10fc8becd68f955bc988389b4d776c6a2926c1b6
Evanson Biwott <509332-evansonbiwott@users.noreply.gitlab.com>
2022-12-10 02:54:57 +0300

    Connect to database and setup routes

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+
+const env = process.env;
 const express = require('express');
 const path = require('path');
 const logger = require('./middleware/logger');
@@ -18,26 +21,36 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 // MongoDB connection URI (Move to env later)
-const dbURI = process.env.dbURI || '';
+const dbURI = `mongodb+srv://${env.DB_USER}:${env.DB_USER_PASS}@${env.DB_CLUSTER}/${env.DB_NAME}?retryWrites=true&w=majority`;
 
 // Get port from the environment else use a default of 3000
-const port = process.env.PORT || 3000;
+const port = env.PORT || 3000;
 
+// Set mongoose strict query to true
+// Suppress deprecation warning on changes coming with mongoose 7
+mongoose.set('strictQuery', true);
 // Connect to mongoDB via mongoose & Listen on port when done
 mongoose
-  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then((result) => {
-    console.log('Successfully connected to DB');
-
     // The idea here is to listen after we have successfully connected to the DB
     // Listening port (Our server has got to listen on some port)
     app.listen(port, () => {
       console.log(`Server running on http://localhost:${port}`);
     });
-  })
-  .catch((error) => {
-    console.log(error);
   });
+
+// Database connection
+db = mongoose.connection;
+db.on('error', (error) => {
+  console.log(error);
+});
+db.once('open', () => {
+  console.log('Successfully connected to the Database');
+});
 
 // Default router
 app.get('/', (req, res) => {
